325d3f2163469f8a1fb4ed2e21035745634dad7d
Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>
2025-11-10 23:16:13 +0530

    fix1
    Signed-off-by: Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -6,13 +6,13 @@ output "baremetal_servers" {
   value = {
     for servers, key in module.baremetal :
     servers => {
-      bms_server_id           = key.baremetal_server_id
-      bms_server_name         = key.baremetal_server_name
-      bms_server_primary_ip   = key.baremetal_server_primary_ip
-      bms_primary_vni_id      = key.baremetal_server_primary_vni_id
-      bms_server_secondary_ip = key.baremetal_server_secondary_ip
-      bms_secondary_vni_id    = key.baremetal_server_secondary_vni_id
-      reserved_ip             = key.reserved_ip
+      bms_server_id                        = key.baremetal_server_id
+      bms_server_name                      = key.baremetal_server_name
+      bms_server_primary_ip                = key.baremetal_server_primary_ip
+      bms_primary_vni_id                   = key.baremetal_server_primary_vni_id
+      bms_server_secondary_ip              = key.baremetal_server_secondary_ip
+      bms_secondary_vni_id                 = key.baremetal_server_secondary_vni_id
+      baremetal_server_primary_reserved_ip = key.baremetal_server_primary_reserved_ip
     }
   }
   description = "IDs and names of the provisioned bare metal servers"
